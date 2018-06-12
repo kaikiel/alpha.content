98c3846912984a61dc79377428b91c7ae5839072
Kiel <kiel@mingtak.com.tw>
2018-06-12 02:47:43 +0000

    update Product add relatedProduct

--- src/alpha/content/content/product.py
+++ src/alpha/content/content/product.py
@@ -6,6 +6,8 @@ from plone.namedfile import field as namedfile
 from plone.supermodel import model
 from plone.supermodel.directives import fieldset
 from z3c.form.browser.radio import RadioFieldWidget
+from z3c.relationfield.schema import RelationChoice, RelationList
+from plone.app.vocabularies.catalog import CatalogSource
 from zope import schema
 from zope.interface import implementer
 from alpha.content import _
@@ -80,6 +82,16 @@ class IProduct(model.Schema):
         description=_(u'ex. clockspeed:100mhz'),
         value_type=schema.TextLine(),
         required=False,
+    )
+
+    relatedProduct = RelationList(
+        title=_(u"Related Product"),
+        default=[],
+        value_type=RelationChoice(
+            title=_(u"Related"),
+            source=CatalogSource(portal_type='Product')
+        ),
+        required=True,
     )
 
 @implementer(IProduct)
