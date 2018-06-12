ad32312fd9e516bd561cc2437835b4e9d9907f33
henry <ah13441673@gmail.com>
2018-06-12 15:50:51 +0000

    add configlet,event

--- src/alpha/content/browser/view.py
+++ src/alpha/content/browser/view.py
@@ -27,7 +27,44 @@ class ProductView(BrowserView):
 	imgBrain = api.content.find(context=context, portal_type='ProductImg', sort_limit=4)
 	return imgBrain
 
+
 class CoverView(BrowserView):
     template = ViewPageTemplateFile('templates/cover_view.pt')
     def __call__(self):
 	return self.template()
+
+
+class UpdateConfiglet():
+    def __call__(self):
+        productBrains = api.content.find(path="gct/products", portal_type="Product")
+        data = {}
+        try:
+            request = self.request
+        except:
+            request = getRequest()
+
+        alsoProvides(request, IDisableCSRFProtection)
+        # data[buggy] = [0,{'1/4': 0, '1/8': 5} ]
+        # data[${cayegory}] = [${category_count}, { ${subject}: ${subject_count} }]
+        try:
+            for item in productBrains:
+                category = item.p_category
+                subject = item.p_subject
+
+                if data.has_key(category):
+                    data[category][0] += 1
+                    if data[category][1].has_key(subject):
+
+
+                        data[category][1][subject] += 1
+                    else:
+                        data[category][1][subject] = 1
+                else:
+                    data[category] = [1, {subject: 1}]
+
+            data = json.dumps(data).decode('utf-8')
+            api.portal.set_registry_record('dict', data, interface=IDict)
+            return "Successful"
+        except  Exception as e:
+            return e
+
