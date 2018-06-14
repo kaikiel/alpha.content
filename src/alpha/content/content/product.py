7cbfe9e89606502b2ff798a7346079242274491c
Kiel <kiel@mingtak.com.tw>
2018-06-14 07:13:31 +0000

    update viewlets timeLimit

--- src/alpha/content/content/product.py
+++ src/alpha/content/content/product.py
@@ -9,15 +9,33 @@ from z3c.form.browser.radio import RadioFieldWidget
 from z3c.relationfield.schema import RelationChoice, RelationList
 from plone.app.vocabularies.catalog import CatalogSource
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from z3c.form import validator
+from plone.directives import form
+import zope.component
+import zope.interface
+from zope.interface import Invalid
+from zope.interface import invariant
 from zope import schema
 from zope.interface import implementer
 from alpha.content import _
+import datetime
+
 
 categoryVocabulary = SimpleVocabulary(
     [SimpleTerm(value=u'mostView', title=_(u'Most View')),
      SimpleTerm(value=u'special', title=_(u'Special')),
      SimpleTerm(value=u'latest', title=_(u'Latest'))]
 )
+
+def rating_constraint(value):
+    if value <= 0 or value > 5:
+        raise Invalid(_(u'Please enter 1 to 5 in the Rating field'))
+    return True
+
+def future_date(value):
+    if value and not value >= datetime.datetime.today():
+        raise Invalid(_(u"Time limit date can not be before today."))
+    return True
 
 
 class IProduct(model.Schema):
@@ -31,23 +49,29 @@ class IProduct(model.Schema):
         required=False
     )
  
-    rating = schema.TextLine(
+    rating = schema.Int(
         title=_(u'Rating'),
         required=False,
-        readonly=True,
+        default=4,
+        constraint=rating_constraint,
     )
 
-    price = schema.TextLine(
+    price = schema.Int(
         title=_(u'Price'),
         description=_(u'Enter USD$'),
-        required=False
+        required=False,
     )
 
-    salePrice = schema.TextLine(
+    salePrice = schema.Int(
         title=_(u'Sale Price'),
         description=_(u'Enter USD$'),
         required=False
     )
+
+    @invariant
+    def price_invariant(data):
+        if data.price < data.salePrice:
+            raise Invalid(_(u'The sale price is bigger than price!'))
 
     cover = namedfile.NamedBlobImage(
         title=_(u'Cover Image'),
@@ -113,7 +137,7 @@ class IProduct(model.Schema):
         required=False,
     )
 
-    fieldset('Index Category', fields=['indexCategory', 'bestSeller'])
+    fieldset('Index Information', fields=['indexCategory', 'bestSeller', 'timeLimit'])
     indexCategory = schema.Choice(
         title=_(u"Index Category"),
         description=_(u'Select the classification of this product (Most View, Special, Latest)'),
@@ -125,6 +149,14 @@ class IProduct(model.Schema):
         title=_(u"Best Seller"),
         required=False,
     )
+
+    timeLimit = schema.Datetime(
+        title=_(u'Time Limit'),
+        description=_(u'If you want to set the time limit, you must put this product in the "Promotions" folder'),
+        constraint=future_date,
+        required=False,
+    )
+    
 
 @implementer(IProduct)
 class Product(Container):
